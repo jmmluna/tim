87c9ba270a73ccc37fac6e5ee77212bd5a853174
Jose Maria Martinez Luna <jmmluna@dap.es>
2021-11-23 15:07:35 +0100

    Printing tests

--- src/main/java/es/jmmluna/tim/controller/EmployeeController.java
+++ src/main/java/es/jmmluna/tim/controller/EmployeeController.java
@@ -1,14 +1,33 @@
 package es.jmmluna.tim.controller;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import com.lowagie.text.DocumentException;
 
 import es.jmmluna.tim.model.Employee;
 import es.jmmluna.tim.service.EmployeeService;
@@ -19,6 +38,9 @@ public class EmployeeController {
 	private static final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
 	@Autowired
 	private EmployeeService employeeService;
+	
+	@Autowired
+	private SpringTemplateEngine templateEngine;
 	
 	@GetMapping("list")
 	public String getEmployees(Model model) {
@@ -46,6 +68,78 @@ public class EmployeeController {
         employeeService.save(employee);
         return "redirect:/employees/list";
     }
+	
+	@GetMapping("print")
+	public ResponseEntity<ByteArrayResource> print(Model model) {
+				
+		ByteArrayOutputStream byteArrayOutputStreamPDF = generatePdfFromHtml(parseThymeleafTemplate());
+		ByteArrayResource inputStreamResourcePDF = new ByteArrayResource(byteArrayOutputStreamPDF.toByteArray());
+
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=informe.pdf").contentType(MediaType.APPLICATION_PDF)
+				.contentLength(inputStreamResourcePDF.contentLength()).body(inputStreamResourcePDF);							
+
+	}
+	
+	
+	
+	private String parseThymeleafTemplate() {
+	    Context context = new Context();
+	    context.setVariable("to", "Baeldung");
+
+	    return templateEngine.process("printTemplate", context);
+	}
+	
+	public void generatePdfFromHtmlToFile(String html) {
+		try {
+	    String outputFolder = System.getProperty("user.home") + File.separator + "thymeleaf.pdf";
+	    LOG.info("###### PDF Generado en: " + outputFolder);
+	    OutputStream outputStream = new FileOutputStream(outputFolder);
+		
+	    ITextRenderer renderer = new ITextRenderer();
+	    renderer.setDocumentFromString(html);
+	    renderer.layout();
+	    renderer.createPDF(outputStream);
+	    
+
+	    outputStream.close();
+		} catch (IOException | DocumentException e) {
+
+			throw new RuntimeException();
+		}
+		
+	}
+	
+	
+	public ByteArrayOutputStream generatePdfFromHtml(String html) {
+		String urlBase = "http://localhost:9080";
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		try {
+			
+				ITextRenderer renderer = new ITextRenderer();
+				renderer.setDocumentFromString(html, urlBase);
+
+				renderer.layout();
+				renderer.createPDF(bos, false);
+				renderer.finishPDF();
+				LOG.info("PDF created correctamente");
+
+			
+			return bos;
+		} catch (DocumentException e) {
+
+			throw new RuntimeException();
+		}finally {
+			if (bos != null) {
+				try {
+					bos.close();
+				} catch (IOException e) {
+					LOG.error("Error creando pdf", e);
+				}
+			}
+		}
+		
+	}
+	
 	
 //	@GetMapping("edit/{id}")
 //    public String showUpdateForm(@PathVariable("id") long id, Model model) {
