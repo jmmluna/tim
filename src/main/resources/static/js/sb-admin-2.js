65fa46f128826b9e3d15279bb0f010eb19bab637
José María Martínez Luna <jmmluna@gmail.com>
2021-11-28 20:55:21 +0100

    Employee delete confirmation

--- src/main/resources/static/js/sb-admin-2.js
+++ src/main/resources/static/js/sb-admin-2.js
@@ -2,7 +2,7 @@
   "use strict"; // Start of use strict
 
   // Toggle the side navigation
-  $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+  $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {	
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
     if ($(".sidebar").hasClass("toggled")) {
@@ -54,3 +54,4 @@
   });
 
 })(jQuery); // End of use strict
+
